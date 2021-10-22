97a3f889161e1f5d2f926e50153d6af489236b66
Paulo David <66272982+QBay2212@users.noreply.github.com>
2021-10-22 16:35:39 -0500

    diseñoLoginBootstrap

--- SIGCA/src/app/app.module.ts
+++ SIGCA/src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {  RouterModule,Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './core/presentation/view/login/login.component';
+
+const routes : Routes =[
+  {path: '', component:LoginComponent},
+  {path: '**', component: LoginComponent},
+]
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ SIGCA/src/app/core/presentation/pages/pages-login/pages-login.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'SIGCA-pages-login',
+  templateUrl: './pages-login.component.html',
+  styleUrls: ['./pages-login.component.css']
+})
+export class PagesLoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
