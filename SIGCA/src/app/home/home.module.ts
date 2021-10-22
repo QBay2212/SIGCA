f30e9fe688c10897cbb2914f3961d53ae8e58027
josueprogram2003 <73667027+josueprogram2003@users.noreply.github.com>
2021-10-22 17:37:48 -0500

    Subiendo nav-top

--- SIGCA/src/app/home/home.module.ts
+++ SIGCA/src/app/home/home.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { NavTopComponent } from './nav-top/nav-top.component';
 
 
 
 @NgModule({
   declarations: [
-    SidebarComponent
+    SidebarComponent,
+    NavTopComponent
   ],
   imports: [
     CommonModule
