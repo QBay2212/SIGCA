72f8a1c0012b51b8c35ac691822e7e38da06084b
Paulo David <66272982+QBay2212@users.noreply.github.com>
2021-10-24 13:25:39 -0500

    login

--- SIGCA/src/app/core/presentation/view/login/login.component.ts
+++ SIGCA/src/app/core/presentation/view/login/login.component.ts
@@ -8,12 +8,23 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
 
+  ingresar( documento: string, contrasena: string ) {
+    if (documento == "71285543" && contrasena == "123456") {
+      this.router.navigate(["/home"],{relativeTo:this.route});
+    } else {
+      alert("DNI y/o contraseña incorrectos")
+    }
+
+    // console.log(documento)
+    // console.log(contrasena)
+  }
+
   constructor(private route:ActivatedRoute, private router:Router) { }
 
   ngOnInit(): void{}
 
-  ingresarCuenta(){
+  // ingresarCuenta(){
 
-      this.router.navigate(["/home"],{relativeTo:this.route});
-  }
+  //     this.router.navigate(["/home"],{relativeTo:this.route});
+  // }
 }
