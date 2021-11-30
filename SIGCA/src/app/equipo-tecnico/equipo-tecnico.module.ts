cf66b5cc59c5ea425ae5cfab03df31681fb459c6
Paulo David <66272982+QBay2212@users.noreply.github.com>
2021-11-29 19:44:18 -0500

    .

--- SIGCA/src/app/equipo-tecnico/equipo-tecnico.module.ts
+++ SIGCA/src/app/equipo-tecnico/equipo-tecnico.module.ts
@@ -1,16 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { RouterModule, Routes } from '@angular/router';
 import { UsuarioRutasComponent } from './usuario-rutas.component';
 import { ReportesRutasComponent } from './reportes/reportes-rutas.component';
 import { ReportesModule } from './reportes/reportes.module';
 import { MaincrearModuloComponent } from './crearModulo/maincrear-modulo/maincrear-modulo.component';
-import { AsignacionRutasComponent } from './asignacion/asignacion-rutas.component';
-import { CrearSeminarioPrincipalComponent } from './crearSeminario/crear-seminario-principal/crear-seminario-principal.component';
-import { CrearSeminarioFormulario2Component } from './crearSeminario/crear-seminario-formulario2/crear-seminario-formulario2.component';
-import { CrearSeminarioFormulario3Component } from './crearSeminario/crear-seminario-formulario3/crear-seminario-formulario3.component';
 
 
 const routes : Routes =[
@@ -21,9 +16,7 @@ const routes : Routes =[
 @NgModule({
   declarations: [
     MaincrearModuloComponent,
-  
-    UsuarioRutasComponent,
-
+    UsuarioRutasComponent
   ],
   imports: [
     CommonModule,FormsModule,RouterModule.forRoot(routes),ReportesModule
