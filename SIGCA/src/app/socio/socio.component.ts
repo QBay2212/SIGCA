bd0daeedf44f1c71dab8c283ff2323cda4d8f98d
Alvaro <alvaroalva@upeu.edu.pe>
2021-12-02 08:24:45 -0500

    progreso

--- SIGCA/src/app/socio/socio.component.ts
+++ SIGCA/src/app/socio/socio.component.ts
@@ -29,7 +29,7 @@ export class SocioComponent implements OnInit {
   ngOnInit(): void {
     this.model.des='Escribir pedido, agradecimiento o necesidad';
     this.listarDistritos();
-    this.Progreso();
+   
     
 
   }
