8eb468b308784ad44005f4f7bdc58b7a290e73bf
ElvisRequejo <73666412+ElvisRequejo@users.noreply.github.com>
2021-12-02 18:49:34 -0500

    navtop

--- SIGCA/src/app/socio/ingresarmodulo/sesiones/sesiones.component.ts
+++ SIGCA/src/app/socio/ingresarmodulo/sesiones/sesiones.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CargarScriptsService } from 'src/app/cargar-scripts.service';
+import { SocioService } from '../../socio.service';
 
 @Component({
   selector: 'SIGCA-sesiones',
@@ -7,7 +9,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SesionesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private pedido:SocioService, private _CargarScripts: CargarScriptsService) {
+    _CargarScripts.Carga(['expotar']);
+
+   }
 
   ngOnInit(): void {
   }
